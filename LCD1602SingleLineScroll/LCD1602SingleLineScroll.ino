d7ef4076580b2b662c8d084719144baaef05a000
Rick <rickqinj@gmail.com>
2020-11-24 12:40:59 -0500

    Upload existed project

---
+++ LCD1602SingleLineScroll/LCD1602SingleLineScroll.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+//LCD screen width
+int screenWidth = 16;
+//Your text
+const String fixedTxt = "CANADA";
+//Original text
+const String originalTxt = "Welcome to Niagara Falls";
+//Scroll text
+String scrollTxt;
+//Display screen
+LiquidCrystal_I2C lcd(0x27, screenWidth, 2);
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {;}
+
+  //Padding 16 space at the end of the scroll text.
+  String paddingSpaces;
+  for (int i = 0; i < screenWidth; i++) {
+    paddingSpaces += " ";
+  }
+  scrollTxt += paddingSpaces;
+  scrollTxt += originalTxt;
+  scrollTxt += paddingSpaces;
+  
+  lcd.init();
+  lcd.backlight();
+}
+
+int startIdx = 0;
+void loop() {
+  lcd.setCursor(5, 1);
+  lcd.print(fixedTxt);
+
+  startIdx = startIdx % (scrollTxt.length() - screenWidth);
+  lcd.setCursor(0, 0);
+  lcd.print(scrollTxt.substring(startIdx, screenWidth + startIdx));
+  startIdx++;
+
+  delay(350);
+}
