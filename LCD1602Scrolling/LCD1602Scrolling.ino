7088bcce9ca4279daa4fab3240b7f699ceba10b2
Rick <rickqinj@gmail.com>
2020-11-24 22:40:09 -0500

    Update LCD1602Scrolling.ino

--- LCD1602Scrolling/LCD1602Scrolling.ino
+++ LCD1602Scrolling/LCD1602Scrolling.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-const char text1[] = "http://letshavefun.epizy";
+const char text1[] = "http://letshavefun.epizy.com";
 const char text2[] = "Welcome to letshavefun!-";
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -11,24 +11,26 @@ void setup() {
   while (!Serial) {;}
   lcd.init();
   lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Arduino");
 }
 
 void loop() {
-  lcd.clear();
-  lcd.setCursor(15, 0);
-  for (int pos = 0; pos < 24; pos++) {
+//  lcd.clear();
+  lcd.setCursor(15, 1);
+  for (int pos = 0; pos < 28; pos++) {
     lcd.print(text1[pos]);
     Serial.println(text1[pos]);
     lcd.scrollDisplayLeft();
     delay(300);
   }
-  lcd.clear();
-  lcd.setCursor(16, 1);
-  for (int pos = 0; pos < 24; pos++) {
-    lcd.print(text2[pos]);
-    Serial.println(text2[pos]);
-    lcd.scrollDisplayLeft();
-    delay(300);
-  }
-  lcd.clear();
+//  lcd.clear();
+//  lcd.setCursor(16, 1);
+//  for (int pos = 0; pos < 24; pos++) {
+//    lcd.print(text2[pos]);
+//    Serial.println(text2[pos]);
+//    lcd.scrollDisplayLeft();
+//    delay(300);
+//  }
+//  lcd.clear();
 }
